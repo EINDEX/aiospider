32a271d15bd1f31e5f6af23cf84afa18720ce46f
EINDEX <snowstarlbk@gmail.com>
2017-12-19 13:39:48 +0800

    no proxy

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='aio_spider',
-        version='0.0.1',
+        version='0.0.3',
         url='https://zhuanlan.zhihu.com/p/26159930',
         license='MIT License',
         install_requires=['aiohttp', 'async-timeout'],
