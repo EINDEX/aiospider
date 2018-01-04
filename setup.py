82895b6c1e1fc52ead5f7ec9f447168c7921d0e0
EINDEX <snowstarlbk@gmail.com>
2018-01-04 15:18:47 +0800

    update aiohttp version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='aiospider',
-        version='0.0.7',
+        version='0.0.8',
         description='A spider use asyncio.',
         license='MIT License',
         install_requires=['cchardet', 'aiodns', 'aiohttp', 'async-timeout', 'aioredis'],
